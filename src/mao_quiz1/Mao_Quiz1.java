cfa3b72a1f65512540dd0dc67f53a54d2d44e414
MaoFan <ferhanelfrenmaopersonal1@gmail.com>
2023-09-04 16:43:17 +0800

    gamay nga nga usab ra

--- src/mao_quiz1/Mao_Quiz1.java
+++ src/mao_quiz1/Mao_Quiz1.java
@@ -14,7 +14,7 @@ public class Mao_Quiz1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hI");
+        System.out.println("hI, hello, hi ,hi");
     }
     
 }
