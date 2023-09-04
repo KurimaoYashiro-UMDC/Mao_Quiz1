41083f8f4a65ebd1178b315b805da273e3aeb7af
MaoFan <ferhanelfrenmaopersonal1@gmail.com>
2023-09-04 16:21:21 +0800

    Version1

---
+++ src/mao_quiz1/Mao_Quiz1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mao_quiz1;
+
+/**
+ *
+ * @author ferha
+ */
+public class Mao_Quiz1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hI");
+    }
+    
+}
